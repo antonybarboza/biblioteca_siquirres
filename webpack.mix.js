30b0bea54acf7ad08c5d833f76ce84c811ed0a99
antonybarboza <66129869+antonybarboza@users.noreply.github.com>
2021-07-14 14:59:03 -0600

    Add files via upload

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+const mix = require('laravel-mix');
+
+
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+     .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps();
